208624524f199fb10df41465d598dd7fa94703de
eske <eskaev@gmail.com>
2017-03-30 15:26:37 +0200

    Removed useless files

--- experiments/spelling_correction/prepare.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# Data from "One Billion Word Benchmark for Measuring Progress in Statistical Language Modeling"
-# (https://arxiv.org/abs/1312.3005)
-# Available at: http://www.statmt.org/lm-benchmark/
--- scripts/recase-and-score.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-scripts/detruecase.perl < $1 > /tmp/source.re
-scripts/score.py /tmp/source.re $2
-rm -f /tmp/source.re
