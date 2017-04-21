555ca8685c368419391d4e8ace299707f5aeebdc
eske <eskaev@gmail.com>
2017-04-21 19:19:39 +0200

    More scripts

--- scripts/select-by-ter.py
+++ scripts/select-by-ter.py
@@ -3,13 +3,15 @@
 import argparse
 import numpy as np
 import random
+import sys
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument('ref_vectors')
 parser.add_argument('vectors')
 parser.add_argument('-n', type=int, default=500000)
-parser.add_argument('-k', type=int, default=10)
+parser.add_argument('-k', type=int, default=1)
+parser.add_argument('-m', type=int, default=1000)
 
 if __name__ == '__main__':
     args = parser.parse_args()
@@ -18,6 +20,7 @@ if __name__ == '__main__':
         ref_vectors = [np.array([float(x) for x in line.split(',')]) for line in f]
     with open(args.vectors) as f:
         vectors = [np.array([float(x) for x in line.split(',')]) for line in f]
+        vectors = list(enumerate(vectors))
 
     n = 0
 
@@ -25,12 +28,19 @@ if __name__ == '__main__':
         vector = ref_vectors[n % len(ref_vectors)]
         n += 1
 
-        v = random.sample(range(len(vectors)), k=10000)
-        v.sort(key=lambda i: np.sum((vector - vectors[i]) ** 2))
+        def key(i):
+            return np.sum((vector - vectors[i][1]) ** 2)
 
-        indices = v[:args.k]
+        indices = random.sample(range(len(vectors)), k=args.m)
 
-        print('\n'.join(map(str, indices)))
+        if args.k > 1:
+            indices = sorted(indices, key=key)[:args.k]
+        else:
+            indices = [min(indices, key=key)]
+
+        for i in indices:
+            sys.stdout.write(str(vectors[i][0]) + '\n')
+        sys.stdout.flush()
 
         for i in sorted(indices, reverse=True):
             del vectors[i]
