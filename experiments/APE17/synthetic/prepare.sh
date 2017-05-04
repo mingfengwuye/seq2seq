54ece792eef4b526ec9d52ee724c70b4a6591f58
eske <eskaev@gmail.com>
2017-05-04 14:16:27 +0200

    Filter script

--- experiments/APE17/synthetic/prepare.sh
+++ experiments/APE17/synthetic/prepare.sh
@@ -5,7 +5,7 @@ data_dir=experiments/APE17/synthetic/data
 raw_data=experiments/APE17/raw_data
 corpus=~/local/data/WMT17/mono/commoncrawl.en
 
-shuf -n 100000 ${corpus} > ${data_dir}/mono.en
+shuf -n 100000 ${corpus} > ${data_dir}/mono.100k.en
 
 ${moses_dir}/bin/lmplz -o 3 < ${data_dir}/mono.en > ${data_dir}/mono.arpa
 ${moses_dir}/bin/lmplz -o 3 < ${raw_data}/train.pe > ${data_dir}/train.arpa
@@ -20,4 +20,4 @@ mkdir ~/local/tmp/
 
 # paste -d' ' ${corpus}.scores1 ${corpus}.scores2 ${corpus} | awk '{printf "%.4f ",-($1+$2)/(NF-2); for(i=3;i<NF;i++) printf "%s ",$i; printf "%s\n",$NF}' > ${corpus}.concat.scores
 paste -d' ' ${corpus}.scores2 ${corpus} | awk '{printf "%.4f",-$1/(NF-1); $1=""; print $0}' > ${corpus}.concat.scores
-LC_NUMERIC=en_US.UTF-8 sort -T ~/local/tmp -g ${corpus}.concat.scores | cut -d' ' -f2- | uniq | head 10000000 > ${corpus}.concat.sorted
+LC_NUMERIC=en_US.UTF-8 sort -T ~/local/tmp -g ${corpus}.concat.scores | cut -d' ' -f2- | uniq | head 10000000 > ${raw_data}/mono.en
